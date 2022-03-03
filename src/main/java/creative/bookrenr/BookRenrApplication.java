babf09114540b172e0baf0a76125d440c7b84120
jae <wogud1514@naver.com>
2022-03-03 16:31:21 +0900

    db connecting

---
+++ src/main/java/creative/bookrenr/BookRenrApplication.java
@@ -0,0 +1,13 @@
+package creative.bookrenr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRenrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRenrApplication.class, args);
+    }
+
+}
